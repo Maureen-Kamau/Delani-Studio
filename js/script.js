48ef6115622cfb2a6f8e017eadd6ca177f50f195
Maureen-kamau <kamaumaureen39@gmail.com>
2022-03-10 23:11:20 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$(document) . ready(function(){
+    $("#design").click(function(){
+      $("p1").toggle("slow");
+      $(".image1").toggle("slow");
+    });
+  
+    $("#development").click(function(){
+      $("p2").toggle("slow");
+      $(".image2").toggle("slow");
+    });
+  
+    $("#product").click(function(){
+      $("p3").toggle("slow");
+      $(".image3").toggle("slow");
+    });
+  
+  });
+  
+  
+  /*Submit section*/
+  $(document).ready(function(){
+    $("#form").submit(function(event){
+      // event.preventDefault();
+      var name = $("input#name").val();
+      var email = $("input#email").val();
+      var message = $("textarea#message").val();
+      if ($("input#name").val() && $("input#email").val()){
+        alert (name + ", we have received your message. Thank you for reaching out to us.");
+      }
+      else {
+        alert("Please enter your name and email!");
+      }
+      
+    });
+  
+  });
